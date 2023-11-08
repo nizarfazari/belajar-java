05117c1eefc3da03dc8ba88c6187f97f7845019d
Muhammad Nizar Fazari <nexsel32@gmail.com>
2023-11-08 20:07:06 +0700

    belajar tipe data string

---
+++ src/dasar_java/TipeDataString.java
@@ -0,0 +1,10 @@
+package dasar_java;
+
+public class TipeDataString {
+    public static void main(String[] args) {
+        String firstName = "Nizar ";
+        String lastName = "Fazari";
+        
+        System.out.println(firstName + " " + lastName);
+    }
+}
