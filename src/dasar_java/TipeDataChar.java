1079f59abcd7abe4ec23356e73bbab91a46d5ad2
Muhammad Nizar Fazari <nexsel32@gmail.com>
2023-11-08 20:02:40 +0700

    belajar tipe character pada java

---
+++ src/dasar_java/TipeDataChar.java
@@ -0,0 +1,11 @@
+package dasar_java;
+
+public class TipeDataChar {
+    public static void main(String[] args) {
+        char n = 'N';
+        char z = 'Z';
+        char r = 'R';
+
+        System.out.println(n);
+    }
+}
