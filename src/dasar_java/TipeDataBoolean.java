deb4a913cf8c6dec0f705bde6562a31f21a7ef61
Muhammad Nizar Fazari <nexsel32@gmail.com>
2023-11-08 20:04:10 +0700

    belajar tipe data boolean pada java

---
+++ src/dasar_java/TipeDataBoolean.java
@@ -0,0 +1,10 @@
+package dasar_java;
+
+public class TipeDataBoolean {
+    public static void main(String[] args) {
+        boolean salah = false;
+        boolean benar = true;
+
+        System.out.println(salah);
+    }
+}
