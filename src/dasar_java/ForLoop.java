389767997baa00d59f60534e9b2b5d210be77cfb
Muhammad Nizar Fazari <nexsel32@gmail.com>
2023-11-09 15:16:40 +0700

    for loop pada java

---
+++ src/dasar_java/ForLoop.java
@@ -0,0 +1,10 @@
+package dasar_java;
+
+public class ForLoop {
+    public static void main(String[] args) {
+
+        for (var counter = 1;counter <= 10; counter ++){
+            System.out.println("perulangan" + counter);
+        }
+    }
+}
