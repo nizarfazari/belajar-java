9db70497ed17e0ea8db3604e2ce5a6b401b5aca3
Muhammad Nizar Fazari <nexsel32@gmail.com>
2023-11-09 15:09:52 +0700

    operasi boolean pada java

---
+++ src/dasar_java/OperasiBoolean.java
@@ -0,0 +1,16 @@
+package dasar_java;
+
+public class OperasiBoolean {
+    public static void main(String[] args) {
+        var absen  = 70;
+        var nilaiAkhir = 70;
+
+
+        boolean lulusAbsen = absen >= 75;
+        boolean lulusNilai = nilaiAkhir >= 75;
+
+
+        var lulus = lulusAbsen && lulusNilai;
+        System.out.println(lulus);
+    }
+}
