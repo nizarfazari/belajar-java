6a02fa65a71bf74354e9c36d2d006fbfd6e31d4d
Muhammad Nizar Fazari <nexsel32@gmail.com>
2023-11-09 15:03:44 +0700

    belajar operasi pada java

---
+++ src/dasar_java/OperasiMatematika.java
@@ -0,0 +1,29 @@
+package dasar_java;
+
+public class OperasiMatematika {
+    public static void main(String[] args) {
+
+        //sama seperti operasi pada bahasa lainnya
+        int a = 100;
+        int b = 10;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+
+        int c  = 100;
+        c += 10;
+
+        System.out.println(c);
+
+        c++;
+        System.out.println(c);
+        // ! yaitu negasi sama dengan pada bahasa lainnya
+        System.out.println(!true);
+
+        
+    }
+
+}
