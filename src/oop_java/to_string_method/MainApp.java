fbe32b80d0f2fc86c56b5420b211c70217e2a93b
Muhammad Nizar Fazari <nexsel32@gmail.com>
2023-11-13 21:22:28 +0700

    belajar toString

---
+++ src/oop_java/to_string_method/MainApp.java
@@ -0,0 +1,8 @@
+package oop_java.to_string_method;
+
+public class MainApp {
+    public static void main(String[] args) {
+        var product = new Product("Apel", 30000);
+        System.out.println(product);
+    }
+}
