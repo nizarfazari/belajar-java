d17afcd43ddee361117497aa2505dc78ab67a285
Muhammad Nizar Fazari <nexsel32@gmail.com>
2023-11-13 20:59:30 +0700

    belajar enscapsulation dan penggunaan getter setter

---
+++ src/oop_java/enscapsulation/MainApp.java
@@ -0,0 +1,9 @@
+package oop_java.enscapsulation;
+
+public class MainApp {
+    public static void main(String[] args) {
+        var categroy = new Category();
+        categroy.setId("2");
+        System.out.println(categroy.getId());
+    }
+}
