c3d261ec6838db75767d5e4fe2c72c4130502592
Muhammad Nizar Fazari <nexsel32@gmail.com>
2023-11-17 20:57:02 +0700

    Method pada Math di java

---
+++ src/java_standart_class/MathApp.java
@@ -0,0 +1,10 @@
+package java_standart_class;
+
+public class MathApp {
+    public static void main(String[] args) {
+        var min = Math.min(100,200);
+        var max = Math.max(100,200);
+
+        System.out.println(min);
+    }
+}
