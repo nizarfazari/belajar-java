68cf38addaf2e61a445055867d0386eb3544d931
Muhammad Nizar Fazari <nexsel32@gmail.com>
2023-11-18 02:14:55 +0700

    belajar Big Integer
    digunakan saat nilainya besar

---
+++ src/java_standart_class/BigNumberApp.java
@@ -0,0 +1,14 @@
+package java_standart_class;
+
+import java.math.BigInteger;
+
+public class BigNumberApp {
+    public static void main(String[] args) {
+        BigInteger a = new BigInteger("100000000");
+        BigInteger b = new BigInteger("100000000");
+
+        BigInteger c = a.add(b);
+
+        System.out.println(c);
+    }
+}
