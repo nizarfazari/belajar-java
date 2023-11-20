987daebdb5139e4e94e4dc59eb10829830540fdc
Muhammad Nizar Fazari <nexsel32@gmail.com>
2023-11-20 22:55:32 +0700

    belajar UUID untuk generate id

---
+++ src/java_standart_class/UUIDApp.java
@@ -0,0 +1,15 @@
+package java_standart_class;
+
+import java.util.UUID;
+
+public class UUIDApp {
+    public static void main(String[] args) {
+        // mengenerate unique id
+        for (var i =0;i <= 100; i++){
+
+            UUID uuid = UUID.randomUUID();
+            String key = uuid.toString();
+            System.out.println(key);
+        }
+    }
+}
