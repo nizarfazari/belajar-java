b8d82bb39698fe460a33dadad0939ee95d347c17
Muhammad Nizar Fazari <nexsel32@gmail.com>
2023-11-20 23:02:24 +0700

    belajar Base64
    untuk meng encode atau decode

---
+++ src/java_standart_class/Base64App.java
@@ -0,0 +1,17 @@
+package java_standart_class;
+
+import java.util.Base64;
+
+public class Base64App {
+    public static void main(String[] args) {
+        String original = "Nizar Faari";
+
+         String encoded =  Base64.getEncoder().encodeToString(original.getBytes());
+         System.out.println(encoded);
+         byte[] bytes = Base64.getDecoder().decode(encoded);
+         // untuk mengkonversi dalam bentuk String
+        
+        String res = new String(bytes);
+        System.out.println(res);
+    }
+}
