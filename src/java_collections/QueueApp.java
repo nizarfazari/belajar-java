29f3a9a8f1c2b901b3c32295d85ed4b2c033d474
Muhammad Nizar Fazari <nexsel32@gmail.com>
2023-11-26 13:58:15 +0700

    belajar queue pada java

---
+++ src/java_collections/QueueApp.java
@@ -0,0 +1,31 @@
+package java_collections;
+
+import java.util.ArrayDeque;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+
+/*
+*   ArrayDeque || LinkedList || PriorityQueue
+*   Array deque menggunakan array sebagai implementasinya
+*   LinkedList menggunakan double linked list sebgai implementasi queue
+*   PriorityQueue menggunakan Array sbg implementasi namaun di urutkan dengan Comparable atau Comparator
+* */
+public class QueueApp {
+    public static void main(String[] args) {
+        Queue<String> queue = new ArrayDeque<>();
+        Queue<String> priorQueue = new PriorityQueue<>();
+
+        queue.add("Nizr");
+        queue.add("Fazari");
+        queue.add("Budi");
+
+        for (String next = queue.poll();next != null; next = queue.poll()){
+            System.out.println(next);
+        }
+
+        System.out.println(queue.size());
+
+
+    }
+}
